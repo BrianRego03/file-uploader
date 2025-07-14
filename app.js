c039d2b26d4cbf9c0b269eb307d1eb981779b10a
brianrego03 <brian.rego03@gmail.com>
2025-07-14 16:48:40 +0530

    Initialize prisma client and prism session storage

--- app.js
+++ app.js
@@ -3,5 +3,29 @@ const path =require("node:path");
 const express =require("express");
 const session =require("express-session");
 const passport =require("passport");
+const{PrismaSessionStore}=require('@quixo3/prisma-session-store');
+import { PrismaClient } from "@prisma/client";
+import prisma from "./db/prismaClient";
 
 const app=express();
+
+app.use(
+    session(
+        {
+            cookie:{
+                maxAge: 24 * 7 * 60 * 60 * 1000
+            },
+            secret:"someStuff",
+            resave:false,
+            saveUninitialized:false,
+            store: new PrismaSessionStore(
+                new PrismaClient(),
+                {
+                    checkPeriod: 2*60*1000,
+                    dbRecordIdIsSessionId:true,
+                    dbRecordIdFunction:undefined,
+                }
+            )
+        }
+    )
+)
