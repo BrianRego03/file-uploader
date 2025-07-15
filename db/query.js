d1df6ff4a19c133c8361b445401409f1dc1d0a9f
brianrego03 <brian.rego03@gmail.com>
2025-07-15 12:09:23 +0530

    Write business logic function to create Users and folders

--- db/query.js
+++ db/query.js
@@ -1,1 +1,38 @@
 import prisma from "./prismaClient";
+
+async function createUser(username,password){
+    const user = await prisma.user.create({
+        data: {
+            name: username,
+            password: password
+        },
+        select:{
+            id:true
+        }
+    })
+
+    await prisma.folder.create({
+        data:{
+            name:"root",
+            userid:user.id
+        }
+    })
+
+    return;
+}
+
+async function createFolder(folderName,parentid){
+    const folder = await prisma.folder.create(
+        {
+            data: {
+                name: folderName,
+                parentid: parentid
+            },
+            select: {
+                id: true
+            }
+        }
+    )
+    return folder.id;
+}
+
