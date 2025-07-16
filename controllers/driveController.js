950190f54ccb80d521725e13be3ead883aafebb7
brianrego03 <brian.rego03@gmail.com>
2025-07-16 10:58:40 +0530

    Send folder data to drive frontend

--- controllers/driveController.js
+++ controllers/driveController.js
@@ -4,6 +4,7 @@ const loadDrive=async(req,res)=>{
     const folderID=req.params.id;
     const folder=await fetchFolderByID(+(folderID));
     console.log(folder);
+    res.render("drive",{folder:folder})
 }
 
 module.exports={loadDrive};
