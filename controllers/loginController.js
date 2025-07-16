5e3cf9eec22f930dcc20ce941a05753b89a8a7fc
brianrego03 <brian.rego03@gmail.com>
2025-07-16 17:28:42 +0530

    Add uploads folder at login if it doesnt exist

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,12 +1,25 @@
 const passport=require("passport");
+const fs = require("fs");
+const path = require("path");
 
 const loadLogin=(req,res)=>{
     res.render("login",)
 }
 
-const userLogin=passport.authenticate("local",{
-    successRedirect:"/",
-    failureRedirect:"/login"
-})
+const ensureUploadsFolder = (req, res, next) => {
+    const uploadsPath = path.join(__dirname, "..", "uploads");
+    if (!fs.existsSync(uploadsPath)) {
+        fs.mkdirSync(uploadsPath);
+    }
+    next();
+};
+
+const userLogin = [
+    ensureUploadsFolder, 
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/login"
+    })
+];
 
 module.exports={userLogin,loadLogin};
