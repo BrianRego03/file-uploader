d01aa297a43af7b2a0a39613d0fa507b218266e7
brianrego03 <brian.rego03@gmail.com>
2025-07-16 16:48:26 +0530

    Set up multer config

--- routers/driveRouter.js
+++ routers/driveRouter.js
@@ -1,10 +1,12 @@
 const {Router}=require("express");
 const { loadDrive,createFolderDrive } = require("../controllers/driveController");
+const upload=require("../config/upload");
 
 
 const driveRouter=Router();
 
 driveRouter.post("/create",createFolderDrive);
+driveRouter.post("/file/create",upload.single('file'),createFileDrive);
 driveRouter.get("/:id",loadDrive);
 
 module.exports=driveRouter;
