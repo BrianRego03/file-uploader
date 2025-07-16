c12437234b197b3969d2641f58369bb938388322
brianrego03 <brian.rego03@gmail.com>
2025-07-16 16:33:02 +0530

    Create toggle file upload button function

--- public/scripts.js
+++ public/scripts.js
@@ -1,10 +1,24 @@
 console.log("scripts.js loaded");
 function toggleForm(){
     console.log("hello")
+    document.getElementById("fileForm").style.display="none";
     if(document.getElementById("folderForm").style.display==="none"){
         document.getElementById("folderForm").style.display="block";
     }else{
         document.getElementById("folderForm").style.display="none";
+    }
+
+}
+
+
+function toggleFileForm(){
+    console.log("hello");
+    document.getElementById("folderForm").style.display="none";
+    if(document.getElementById("fileForm").style.display==="none"){
+        
+        document.getElementById("fileForm").style.display="block";
+    }else{
+        document.getElementById("fileForm").style.display="none";
     }
 
 }
